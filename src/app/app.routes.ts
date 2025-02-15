528dd6809adffcd942e769772c176c30895923b8
Likeur <jacksimba28@gmail.com>
2025-02-15 21:35:02 +0200

    add login and register component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        title: 'Angular 19 starter repos',
+        loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
+    },
+    {
+        path:'login',
+        title: 'log in component',
+        loadComponent: () => import('./components/auth/login/login.component').then(m => m.LoginComponent)
+    },
+];

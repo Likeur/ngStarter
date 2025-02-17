6e207b41caab050e78bedea769bad72df7beb084
Likeur <jacksimba28@gmail.com>
2025-02-17 18:34:34 +0200

    remove deprecated addListener method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,39 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styles: [],
 })
-export class AppComponent {
-  title = 'ngStarter';
+export class AppComponent implements OnInit {
+  
+
+  isDarkMode: boolean = false;
+
+  private document: Document = inject(DOCUMENT);
+
+  updateTheme() {
+    // Ajout du darkmode selon le theme du systeme light ou dark
+    if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: dark)').matches
+    ) {
+      document.documentElement.classList.add('dark');
+      this.isDarkMode = true;
+      localStorage.setItem('darkMode', this.isDarkMode.toString());
+    } else if (
+      window.matchMedia &&
+      window.matchMedia('(prefers-color-scheme: light)').matches
+    ) {
+      document.documentElement.classList.remove('dark');
+      this.isDarkMode = false;
+      localStorage.setItem('darkMode', this.isDarkMode.toString());
+    }
+  }
+
+  ngOnInit() {
+    
+
+    // Appeler la fonction une première fois pour définir le thème initial
+    this.updateTheme();
+
+    // Écouter les changements de thème
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', this.updateTheme);
+  }
 }
